26bb463d917b83f9751d1adca58371a05a01b1d4
ReidBarbeln2022 <814684@mystma.org>
2020-12-17 16:39:05 +0000

    top 5 movies try 1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(movie1, movie2, movie3, movie4, movie5):
+    return movie1 + movie2 + movie3 + movie4 + movie5
+
+print (topFiveMovies(" shrek ", " shreck 2", " shrek 3", " shrek 4", " shrek 5"))
